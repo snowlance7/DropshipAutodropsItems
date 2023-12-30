4e026e345249b78b2a3103f3d5faf3ba0f9c0f4b
Snowlance <snowlance7@gmail.com>
2023-12-30 14:48:48 -0600

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using GameNetcodeStuff;
+using UnityEngine;
+
+namespace DropshipAutodropsItems
+{
+    public class Plugin
+    {
+
+    }
+}
